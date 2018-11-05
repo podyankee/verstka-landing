d357de390fbcde10abd18344b2f6b817f942244e
podyankee <ukon_2004@mail.ru>
2018-11-05 21:14:59 +0300

    Updates

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    
+
+
+    });
+    
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiJChkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24oKXtcclxuICAgIFxyXG5cclxuXHJcbiAgICB9KTtcclxuICAgICJdfQ==
